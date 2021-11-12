c29ff48e694063a34b2b6bbedca90a615006677c
Faisca12 <ricardofaisca12@gmail.com>
2021-11-12 16:29:51 +0000

    .

--- firebase.js
+++
@@ -1,5 +0,0 @@
-<!--------------------------FIRBASE LIBRARIES-------------------------------------------------->
-<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-database.js"></script> 
-<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-storage.js"></script> 
